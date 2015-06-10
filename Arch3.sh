102f6125dead02b3a6631dbaaa843b5353e322fb
TimoStraus <timo-fluss@web.de>
2015-06-10 19:51:52 +0200

    Arch script noch etwas ausgebessert.

---
+++ Arch3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash         
+
+time = read webblockerArchmodul2.sh
+
+#nmcli dev disconnect enp1s0f0
+
+sh ~/Output/Programme/webblocker/webblockerArchmodul2.sh &
+
+#counter() {
+#sleep 10;  time=`expr $time - 10`
+#}
+
+#while [ $time -gt 0 ]
+#	do
+#		counter
+#	done
+	
+#sleep 5;	
+#nmcli dev connect enp1s0f0
+#sleep 5;
+
+#exit
+
