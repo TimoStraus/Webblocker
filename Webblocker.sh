8d63f450955317fb16842b71c5e958458ac192d3
TimoStraus <timo-fluss@web.de>
2015-06-10 10:41:15 +0200

    Update Webblocker.sh

--- Webblocker.sh
+++ Webblocker.sh
@@ -1,25 +1,35 @@
 #!/bin/bash         
 
-#The following line uses Zenity to show a dialog:
-time=$(zenity --entry --text "How many minutes do you want to be offline?")
-#time=$(kdialog --title "Offline Time" --inputbox "How many minutes do you want to be offline?" "60")
 
+#achtung, das skript ist jetzt für KDE optimiert!
+#die nächste zeile kann verwendet werden, wenn zenity benutzt wird:
+
+#time=$(zenity --entry --text "How many minutes do you want to be offline?")
+#time=$(kdialog --title "Offline Time" --inputbox "How many minutes do you want to be offline?" "60")
+echo 'How many minutes do you want to be offline?'
+
+read time
 
 time=`expr $time \* 60`
 
-#disconnecting the internet with nmcli:
-nmcli nm enable false
+nmcli dev disconnect enp1s0f0 &
+
+counter() {
+sleep 10;  time=`expr $time - 10`
+}
 
 while [ $time -gt 0 ]
 	do
-		sleep 10;  time=`expr $time - 10`
+		counter
 	done
 	
-sleep 5; 	
-nmcli nm enable true
+sleep 5;	
+nmcli dev connect enp1s0f0 &
 sleep 5;
-zenity --info --text="You can now connect to the internet again!"
+#zenity --info --text="You can now connect to the internet again!"
 #kdialog --msgbox "You can now browse again!"
 
 exit
+
+
 
