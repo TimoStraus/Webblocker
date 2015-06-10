678dcbec246ca91d2a2f6011ef5d5dbd79432afc
TimoStraus <timo-fluss@web.de>
2015-06-10 19:48:01 +0200

    weiss nicht mehr was getauscht wurde

--- Webblocker.sh
+++ Webblocker.sh
@@ -1,9 +1,13 @@
-#!/bin/bash         
+ #!/bin/bash         
 
 #The following line uses Zenity to show a dialog:
 time=$(zenity --entry --text "How many minutes do you want to be offline?")
+
+#if you are using kdialog, use the following line instead:
 #time=$(kdialog --title "Offline Time" --inputbox "How many minutes do you want to be offline?" "60")
 
+#if you want to run this program in a terminal, use the following line:
+echo 'How many minutes do you want to be offline?'
 
 time=`expr $time \* 60`
 
