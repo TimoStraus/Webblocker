2f769010abb4f79bba9506b32bad105cbd0716a8
TimoStraus <timo-fluss@web.de>
2015-06-30 19:29:35 +0200

    zum löschen

--- Arch1.sh
+++ Arch1.sh
@@ -2,26 +2,28 @@
 
 echo 'How many minutes do you want to be offline?'
 
-read time
+if [ -x "$1" ]; then
 
-time=`expr $time \* 60`
+	#time=`expr $time \* 60`
+	time=60
 
-nmcli dev disconnect enp1s0f0
+	nmcli dev disconnect enp1s0f0
 
-#sh ~/Output/Programme/webblocker/webblockerArchmodul2.sh &
+	#sh ~/Output/Programme/webblocker/webblockerArchmodul2.sh &
 
-counter() {
-sleep 10;  time=`expr $time - 10`
-}
+	counter() {
+	sleep 10;  time=`expr $time - 10`
+	}
 
-while [ $time -gt 0 ]
+	while [ $time -gt 0 ]
 	do
 		counter
 	done
 	
-sleep 5;	
-nmcli dev connect enp1s0f0
-sleep 5;
+	sleep 5;	
+	nmcli dev connect enp1s0f0
+	sleep 5;
 
-exit
+	exit
 
+fi
